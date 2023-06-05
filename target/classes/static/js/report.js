a2c8a0a70ef1bda9261c439911e5a889678dbc25
Diego <diegobqu@ucm.es>
2023-06-05 17:32:17 +0200

    gitignore added

--- target/classes/static/js/report.js
+++
@@ -1,9 +0,0 @@
-function reportUser(userTarget, description = ""){
-    description = document.getElementById('report-desc').value;
-    go(`/user/${userTarget}/report`, "POST", {description}, false)
-        .then(d => {console.log(d);
-            toggleReportForm();})
-        .catch(e => {console.log(e)
-            alert("Something went wrong.");
-            });
-}
